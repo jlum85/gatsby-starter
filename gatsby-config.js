82d5103bdbfe2ca3afb7ac555917a2f8b902ba02
jlum85 <jlum@wanadoo.fr>
2020-05-07 17:22:39 +0200

    query data from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 module.exports = {
   /* Your site config here */
+  /*  pour graph ql , ce sera dans   site /  siteMetadata  */
+
+  siteMetadata: {
+    title: "My Gatsby blog",
+  },
 
   plugins: ["gatsby-plugin-sass"],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Title from "../components/title"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <Title text="Welcome" />
+    <Title text={data.site.siteMetadata.title} />
     <div>
       <Link to="/">Home</Link> | <Link to="/about">About me</Link>
     </div>
     <p> This is the index page</p>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
