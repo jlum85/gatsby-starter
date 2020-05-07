d9c5878ec4d6426f61b4f65d28450940a84c9e2e
jlum85 <jlum@wanadoo.fr>
2020-05-07 14:53:51 +0200

    first commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,10 @@ import { Link } from "gatsby"
 
 export default () => (
   <div>
-    <h1>This is the index page</h1>
+    <h1>This is the about page</h1>
     <div>
       <Link to="/">Home</Link> | <Link to="/about">About me</Link>
     </div>
-    <p> This is the index page</p>
+    <p> his is the about page</p>
   </div>
 )
